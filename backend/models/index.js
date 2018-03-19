f375a6779055e8a68052fd29018fb17b24e5ebc6
Yong-L <dkrcjs7521@gmail.com>
2018-03-19 09:08:56 -0400

    Fixed deprecation warning

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,9 +7,10 @@ const env       = process.env.NODE_ENV || 'development';
 const config    = require(path.join(__dirname, '..', 'config', 'config.js'))[env]
 const db        = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config, {
-  timestamps: false
-});
+config.operatorsAliases = false;
+config.timestamps = false;
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
