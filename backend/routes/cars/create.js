3a4a23ed6f3a4e0e2e29de175cf21b568b7d9f4d
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2018-03-18 22:50:12 -0400

    Add/Update car works with PUT request

--- backend/routes/cars/create.js
+++ backend/routes/cars/create.js
@@ -4,12 +4,8 @@ const verifyToken  = require('../../auth/verifyToken');
 module.exports = (db, express, createToken) => ({
   router() {
     const router = express.Router();
-    router.post('/',verifyToken, this.createCar);
-    router.get('/', verifyToken, this.get);
+    router.put('/',verifyToken, this.createCar);
     return router;
-  },
-  get(req, res) {    
-    res.status(202).json({success: false,userID:req.userID ,msg: 'The request has been accepted for processing. CreateCar implimintation not complete.'});
   },
 
   createCar(req, res) {
@@ -21,20 +17,51 @@ module.exports = (db, express, createToken) => ({
           id: req.id
         }
       }).then(user => {
-        db.cars.create({
-          user_id: user.id,
-          size: req.body.size,
-          make: req.body.make,
-          model: req.body.model,
-          color: req.body.color
-        }).then((car) => {
-            res.status(200).json({success: true, msg: 'Car created'});
-        }).catch(() => {
-            res.status(400).json({success: false, msg: 'Failed to create a car'});
+        db.cars.find({
+          attributes: ['user_id', 'size', 'make','model','color'], //sequelize added a invalid data field to the query
+          where:{
+            user_id: user.id,
+          }
+        }).then(oldCar => {
+          console.log(!oldCar);
+          if(!oldCar){
+            db.cars.create({
+              user_id: user.id,
+              size: req.body.size,
+              make: req.body.make,
+              model: req.body.model,
+              color: req.body.color
+            }).then((car) => {
+                res.status(200).json({success: true, msg: 'Car created'});
+            }).catch(() => {
+                res.status(400).json({success: false, msg: 'Failed to create a car'});
+            })
+            
+          }
+          else{
+            db.cars.update({
+              size: req.body.size,
+              make: req.body.make,
+              model: req.body.model,
+              color: req.body.color
+            },
+            {
+              where:{
+                user_id: user.id,
+              },
+              returning: true,
+            }).then((car) => {
+                res.status(200).json({success: true, msg: 'Car updated'});
+            }).catch(() => {
+                res.status(400).json({success: false, msg: 'Failed to update a car'});
+            })
+          }
+        }).catch(()=>{
+          res.status(400).json({success: false, msg: 'Failed to create a car'});
         })
       }).catch(() => {
         //Good token but no user found?????
-        res.status(404).json({success: false, msg: 'You seem to be lost...'});  
+        res.status(404).json({success: false, msg: 'You seem to be lost... Are you sure you are logged in?'});  
       });
     }
   }
