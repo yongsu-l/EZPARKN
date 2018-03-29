df8faaac3e5fe8d3489184705fbe86c9e4db6a4d
Yong-L <dkrcjs7521@gmail.com>
2018-03-29 17:01:53 -0400

    Yellow Port number

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ const logger        = require('morgan');
 const cookieParser  = require('cookie-parser');
 const bodyParser    = require('body-parser');
 const env           = process.env.NODE_ENV || 'development';
-const port          = process.env.PORT || 3001;
+const port          = process.env.PORT || '3001';
 const config        = require('./config/config')[env];
 const colors        = require('colors');
-
-const app = express();
+const app           = express();
+const server        = require('http').Server(app);
+const io            = require('socket.io')(server);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./routes'));
 
 app.listen(port, () => {
-  console.log("Server is up and running on port ".green + port);
+  console.log("Server is up and running on port ".green + port.yellow);
 });
 
 module.exports = app;
