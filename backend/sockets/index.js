52cdb53ab49ffc1053677fc4cb514544ef3026f9
Yong-L <dkrcjs7521@gmail.com>
2018-04-17 15:56:25 -0400

    Sockets indexjs

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -1,9 +1,16 @@
 // sockets/index.js
-module.exports = function Server(io) {
+
+module.exports = function Server(io, db) {
+  
+  let userCount = 0;
+
   io.on('connection', socket => {
     console.log('New client connected');
 
+    userCount++;
+
     socket.on('disconnect', () => {
+      userCount--;
       console.log('User disconnected');
     });
   });
