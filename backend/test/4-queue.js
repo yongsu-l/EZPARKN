c012b334afa5d715cebe973f05ee3390802a303c
Yong-L <dkrcjs7521@gmail.com>
2018-04-18 07:51:32 -0400

    Test for Queue socket

--- backend/test/4-queue.js
+++ backend/test/4-queue.js
@@ -7,23 +7,43 @@ describe('Socket IO Queue Connection', function() {
 	beforeEach(function(done) {
 		sender = io(socketURL);
 		receiver = io(socketURL);
+    notreceiver = io(socketURL);
 		done();
 	});
 
 	afterEach(function(done) {
 		sender.disconnect();
 		receiver.disconnect();
+    notreceiver = io(socketURL);
 		done();
 	});
 
-	describe('Basic Message Event', function(){
-		it('Clients should receive a message when the `message` event is emited.', function(done){
-			sender.emit('message', testMsg);
-			receiver.on('message', function(msg){
-				expect(msg).to.equal(testMsg);
-				done();
-			});
-		});
-	});
+	describe('Joining queue room', function(){
 
+		it('Client should be able to join the room of queue when requested', function(done){
+
+      // First park a user
+      sender.emit('park', {lat: 1, long: 1, userId: 2});
+      sender.emit('leaving parking', {leavingTime: Date.now(), id: 1});
+
+      sender.on('updated parking', function(data) {
+        console.log(data);
+        expect(data.leavingTime).to.not.be.null;
+      });
+
+      notreceiver.on('new parking spots', function(spots) {
+        // Those who didn't subscribe to queue should not receive anything so this would automatically throw error
+        expect.to.be.empty(spots);
+      });
+
+      // The clients who are in the queue should now be able to get an emit
+      // which is triggered when a car leaves;
+      receiver.emit('enter queue', {});
+      receiver.on('new parking spots', function(spots) {
+        expect(spots).to.not.be.empty;
+        done();
+      });
+		});
+
+	});
 });
