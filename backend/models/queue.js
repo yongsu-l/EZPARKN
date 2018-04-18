4f5f6f268634bfea48fdffa35a072fecfa04f441
Yong-L <dkrcjs7521@gmail.com>
2018-04-17 23:56:47 -0400

    Rewrote queues for clients who are waiting for parking

--- backend/models/queue.js
+++ backend/models/queue.js
@@ -1,8 +1,8 @@
 // models/queue.js
 
 module.exports = (sequelize, Sequelize) => {
+
   const Queue = sequelize.define('queues', {
-     
     id: {
       allowNull: false,
       autoIncrement: true,
@@ -10,21 +10,18 @@ module.exports = (sequelize, Sequelize) => {
       unique: true,
       type: Sequelize.INTEGER
     },
-    msg: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
     time: {
       allowNull: false,
-      type: Sequelize.DATE
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOWW
     },
-    parkingId: {
+    userId: {
       allowNull: false,
       unique: true,
       type: Sequelize.INTEGER,
       references: {
         model: {
-          tableName: 'parkings'
+          tableName: 'users'
         },
         key: 'id'
       }
