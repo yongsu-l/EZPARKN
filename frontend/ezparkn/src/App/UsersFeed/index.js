aa0c0c84851d568034e1693af05bdb778534446f
Steven <stevenbarrios03@gmail.com>
2018-04-29 20:17:41 -0400

    Navbar button and switch event

--- frontend/ezparkn/src/App/UsersFeed/index.js
+++ frontend/ezparkn/src/App/UsersFeed/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import 'style.css';
+import './style.css';
 
 export default class UsersFeed extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     // Render nothing if the "show" prop is false
     if(!this.props.show) {
@@ -27,10 +28,31 @@ export default class UsersFeed extends Component {
                   <small>Range Rover</small>
                 </div>
                 <div className="pl-20">
-                  <button className="btn btn-primary">Get</button>
+                  <button className="btn btn-sm btn-success">Get</button>
                 </div>
               </div>
             </li>
+            {
+              this.props.feed.map(function(err,response){
+                <li className="list-group-item">
+                  <div className="user">
+                    <div className="pr-20">
+                      <a className="avatar avatar-leaving">
+                        <img className="img-fluid" src="../../../img/champagne_papi.png"/>
+                        <i></i>
+                      </a>
+                    </div>
+                    <div className="user-body">
+                      <p className="h5">Drake <span> Leaving in 10 min</span></p>
+                      <small>Range Rover</small>
+                    </div>
+                    <div className="pl-20">
+                      <button className="btn btn-sm btn-success">Get</button>
+                    </div>
+                  </div>
+                </li>
+              })
+            }
           </ul>
         </div>
       </div>
