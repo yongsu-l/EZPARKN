e89f4c4f62f691b7c2796794beb313d09902f9a6
barriosx <stevenbarrios03@gmail.com>
2018-05-02 21:27:55 -0400

    Feed component update

--- frontend/ezparkn/src/App/UsersFeed/index.js
+++ frontend/ezparkn/src/App/UsersFeed/index.js
@@ -1,61 +1,32 @@
 import React, { Component } from "react";
+import SingleUser from './SingleUser';
 import './style.css';
 
 export default class UsersFeed extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
   }
 
   render() {
+    let Users = this.props.feed.map( (user) => {
+      return(
+        <SingleUser name={user.name} leavingIn={user.leavingIn}/>
+      );
+    });
     // Render nothing if the "show" prop is false
     if(!this.props.show) {
       return null;
     }
     return (
-      <div className="form-group row USERS_LEAVING">
-        <div className="col-md-12">
-          <ul className="list-group">
-            <li className="list-group-item">
-              <div className="user">
-                <div className="pr-20">
-                  <a className="avatar avatar-leaving">
-                    <img className="img-fluid" src="../../../img/champagne_papi.png"/>
-                    <i></i>
-                  </a>
-                </div>
-                <div className="user-body">
-                  <p className="h5">Drake <span> Leaving in 10 min</span></p>
-                  <small>Range Rover</small>
-                </div>
-                <div className="pl-20">
-                  <button className="btn btn-sm btn-success">Get</button>
-                </div>
-              </div>
-            </li>
-            {
-              this.props.feed.map(function(err,response){
-                <li className="list-group-item">
-                  <div className="user">
-                    <div className="pr-20">
-                      <a className="avatar avatar-leaving">
-                        <img className="img-fluid" src="../../../img/champagne_papi.png"/>
-                        <i></i>
-                      </a>
-                    </div>
-                    <div className="user-body">
-                      <p className="h5">Drake <span> Leaving in 10 min</span></p>
-                      <small>Range Rover</small>
-                    </div>
-                    <div className="pl-20">
-                      <button className="btn btn-sm btn-success">Get</button>
-                    </div>
-                  </div>
-                </li>
-              })
-            }
-          </ul>
+      <div className="card">
+        <div className="card-body">
+          <div className="form-group row USERS_LEAVING">
+            <div className="col-md-12">
+              <ul className="list-group">{Users}</ul>
+            </div>
+          </div>
         </div>
-      </div>
+    </div>
     )
   }
 }
