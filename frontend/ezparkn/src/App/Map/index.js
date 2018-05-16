f5ad9fa6d8e7b7d76b9c3bcae6b26d17c371a519
Mikhail-Kreytser <mikhail.kreytser@gmail.com>
2018-05-16 11:34:06 -0400

    missed change

--- frontend/ezparkn/src/App/Map/index.js
+++ frontend/ezparkn/src/App/Map/index.js
@@ -67,7 +67,7 @@ export default class Map extends Component {
           {this.props.parkingSpots
         .map((parkingSpot, index) => {
                 return (
-                  <InfoWindow lat={parkingSpot.lat}  lng={parkingSpot.long} content={parkingSpot.user.username} onCloseClick={this.onCloseClick} onClick={this.onClick}/> 
+                  <InfoWindow lat={parkingSpot.lat}  lng={parkingSpot.long} content={parkingSpot.user?parkingSpot.user.username:"Anonymous"} onCloseClick={this.onCloseClick} onClick={this.onClick}/> 
                 )
             })}
 
