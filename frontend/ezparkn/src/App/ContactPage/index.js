0f7fb98cfc11d0316fd97d383d70faf5ed9282ed
Stephen McDonald <stephenmcdonald@Stephens-MacBook-Pro.local>
2018-05-16 17:27:16 -0400

    simple contact page

--- frontend/ezparkn/src/App/ContactPage/index.js
+++ frontend/ezparkn/src/App/ContactPage/index.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
-
+import './style.css'
 class ContactPage extends React.Component{
-
+ constructor(props) {
+    super(props);
+  }
   render(){
-    return (
 
-      <div> 
-        <form>
-      
-        <label for="fname">First Name</label>
-       <input type="text" id="fname" name="firstname" placeholder="Your name.." />
-        <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
-        <label for="country">Country</label>
-        <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
+  return (
+
+      <div class="container"> 
+      <div>
+          <button><a href="/main">Go Back</a></button>
+          </div>
+      <form>
+        <label htmlFor="fname">First Name</label>
+       <input type="text" id="fname" name="firstname" placeholder="First name.." />
+        <label htmlFor="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Last name.." />
+
+        <label htmlFor="users">Choose One</label>
+        <select id="users" name="user">
+          <option value="student">Student</option>
+          <option value="faculty">Faculty</option>
+          <option value="other">Other</option>
         </select>
 
-        <label for="subject">Subject</label>
-        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-        <input type="submit" value="Submit" />
-
-        </form>
+        <label htmlFor="subject">Message</label>
+        <textarea id="message" name="message" placeholder="Any Message"></textarea>
+       
+       <input type="submit" value="Submit" />
+      </form>
+      
       </div>
     
     
